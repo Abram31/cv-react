348f93eee05873cf96ddb010a40c6b4bbe7a8b9e
Abram31 <a447623993@gmail.com>
2022-09-14 11:09:29 +0300

    feat: add header

---
+++ src/header/header.jsx
@@ -0,0 +1,19 @@
+import {Component} from "react";
+
+
+class Navigation extends Component {
+    render() {
+        return (
+            ['About myself', 'Contacts', ]
+        )
+    }
+}
+
+
+class Header extends Component {
+    render() {
+        return <section>
+            header
+        </section>
+    }
+}
