6b37a2ad2dda606bfca02bf4f4d0173cc9582447
Abram31 <a447623993@gmail.com>
2022-09-27 12:22:40 +0300

    fix: add more information to my history

--- src/my-history/my-history.jsx
+++ src/my-history/my-history.jsx
@@ -2,11 +2,17 @@ import './my-history.scss'
 
 export const MyHistory = () => {
     return (
-        <section className='my-history'>
+        <section className='my-history'>   
             <h3> {'My history as JavaScript developer'.toLocaleUpperCase()}</h3>
-            <p>Since September 2021, I began to study JavaScript and other systems, frameworks necessary for the JavaScript Developer profession.</p>
-            <p> From May 2022 to September 2022, I worked for PROTUTOR company. I was engaged in the support of sites company, development, improvement of pages 
-                for applications and did the migration of applications to REACT. At the same time, I continued my studies at the Rolling Scopes School. </p>
+            <p> Since 2019, I began to self-study JavaScript and other systems, frameworks necessary for the profession of a JavaScript developer.</p>
+            <p>From September 2021 I started to study JavaScript at The Rolling Scopes School.</p>
+            <p> From May 2022 to September 2022, I worked for PROTUTOR company. I was engaged in the support of sites company, development, improvement of pages
+                for applications and did the migration of applications to REACT. Our team consisted of two frontend developers and one backend with whom we actively interacted. At the same time, I continued my studies at The Rolling Scopes School.</p>
+
+            <p>At the final JAVASCRIPT/FRONT-END task of The Rolling Scopes School, I was the team leader of our team. In addition to writing code, my duties included drawing up work plans and monitoring compliance with the deadlines for completing tasks by all team members. 
+                I also controlled that all merges were executed only after all team members had approved the pull request.
+                For me its was realy important and interesting expirience. </p>
+
             <p> Nowadays I am currently open to work as a JavaScript developer</p>
         </section>
     )
